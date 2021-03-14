7ab9563238a8febd4e9a667212fb2431125f86fe
Felipe Stuart <felipe5stuart@gmail.com>
2021-03-14 02:26:16 -0300

    agregue el celcius a los grados

--- client/src/components/WeatherCard.js
+++ client/src/components/WeatherCard.js
@@ -13,9 +13,9 @@ const WeatherCard = (props) => {
       </Card.Body>
       <ListGroup className='list-group-flush'>
         <ListGroupItem>Today Weather: {props.main}</ListGroupItem>
-        <ListGroupItem>Temperature: {props.temp}º</ListGroupItem>
-        <ListGroupItem>Max temperature: {props.max}º</ListGroupItem>
-        <ListGroupItem>Min temperature: {props.min}º</ListGroupItem>
+        <ListGroupItem>Temperature: {props.temp}ºC</ListGroupItem>
+        <ListGroupItem>Max temperature: {props.max}ºC</ListGroupItem>
+        <ListGroupItem>Min temperature: {props.min}ºC</ListGroupItem>
         <ListGroupItem>Humidity: {props.hum}%</ListGroupItem>
         <ListGroupItem>Wind: {props.wind}km/h</ListGroupItem>
       </ListGroup>
