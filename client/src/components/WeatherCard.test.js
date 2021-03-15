10196fe84401b97ac0cb94a234c9988f3ad0e6df
Felipe Stuart <felipe5stuart@gmail.com>
2021-03-15 03:36:10 -0300

    added one more test

--- client/src/components/WeatherCard.test.js
+++ client/src/components/WeatherCard.test.js
@@ -1,9 +1,18 @@
 import React from 'react'
-import {render} from '@testing-library/react'
+import {render, cleanup} from '@testing-library/react'
 import WeatherCard from './WeatherCard'
+import "@testing-library/jest-dom"
+
+afterEach(cleanup)
 
 it("renders correctly", () => {
     const {queryByTestId } = render(<WeatherCard/>)
 
     expect(queryByTestId("weather-card")).toBeTruthy()
+})
+
+it("renders the title with city prop", () => {
+    const {queryByTestId } = render(<WeatherCard city='london'/>)
+
+    expect(queryByTestId("weather-card")).toHaveTextContent('london')
 })
